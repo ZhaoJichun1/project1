14a2a329355d979f350f3448982449e087b135e3
ZhaoJichun1 <101465510+ZhaoJichun1@users.noreply.github.com>
2022-04-08 21:24:07 +0800

    PCA for parameter visualization

---
+++ PCA.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def pca(data):
+    n = 3
+    mean = np.mean(data, axis=0)
+    zero_mean_data = data - mean
+    cov = np.cov(zero_mean_data, rowvar=False)
+    value, vec = np.linalg.eig(cov)
+    index = np.argsort(value)
+    n_index = index[-n:]
+    n_vec = vec[:, n_index]
+    low_dim_data = np.dot(zero_mean_data, n_vec)
+    return low_dim_data
